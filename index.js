87eac2fe52e392ef927cd2ae4d28e6fd757d43c4
IhsanKirson <mnurihsamutaqin66.siswa@smkn4bdg.sch.id>
2022-04-04 16:49:26 +0700

    Add debug mode

--- index.js
+++ index.js
@@ -8,53 +8,57 @@ var cron = require('node-cron');
 console.log('App is running');
 
 const twitterApi = new TwitterApi({
-  appKey: process.env.TWITTER_API_KEY,
-  appSecret: process.env.TWITTER_API_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-const getTime = async () =>  {
-  let result = {date: '', time: ''};
-  await axios.get('http://api.aladhan.com/v1/timingsByCity?city=Bandung&country=Indonesia')
-    .then(res => {
-      result.date = res.data.data.date.gregorian.date;
-      result.time = res.data.data.timings.Maghrib;
-    });
+const getTime = async () => {
+    let result = { date: '', time: '' };
+    await axios.get('http://api.aladhan.com/v1/timingsByCity?city=Bandung&country=Indonesia')
+        .then(res => {
+            result.date = res.data.data.date.gregorian.date;
+            result.time = res.data.data.timings.Maghrib;
+        });
 
-  return result;
+    return result;
 }
 
 const createDiff = (date, time) => {
-  date = date.split('-');
-  time.split(':')
-  var today = new Date();
-  var target = new Date(date + ' ' + time);
-  var diffMs = (target - today); // milliseconds between now & target
-  var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
-  var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
-  return diffHrs + ' jam ' + diffMins + ' menit';
+    date = date.split('-');
+    time.split(':')
+    var today = new Date();
+    var target = new Date(date + ' ' + time);
+    var diffMs = (target - today); // milliseconds between now & target
+    var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
+    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+    return diffHrs + ' jam ' + diffMins + ' menit';
 }
 
-var task = cron.schedule('* * * * *', () =>  {
-  getTime().then((res) => {
-    result = createDiff(res.date, res.time);
+if (process.env.APP_DEBUG) {
+    console.log('App is on debug mode');
+} else {
+    cron.schedule('* * * * *', () => {
+        getTime().then((res) => {
+            result = createDiff(res.date, res.time);
 
-    let arr = result.split(' ');
-    result +=' menuju Maghrib untuk Bandung';
-  
-    if (arr[2] < 0) {
-      twitterApi.v2.post(
-        'account/update_profile.json',
-        { name: 'Udah buka ya?' },
-        { prefix: 'https://api.twitter.com/1.1/' },
-      );
-    } else {
-      twitterApi.v2.post(
-        'account/update_profile.json',
-        { name: result },
-        { prefix: 'https://api.twitter.com/1.1/' },
-      );
-    }
-  });
-});
+            let arr = result.split(' ');
+            result += ' menuju Maghrib untuk Bandung';
+
+            if (arr[2] < 0) {
+                twitterApi.v2.post(
+                    'account/update_profile.json',
+                    { name: 'Udah buka ya?' },
+                    { prefix: 'https://api.twitter.com/1.1/' },
+                );
+            } else {
+                twitterApi.v2.post(
+                    'account/update_profile.json',
+                    { name: result },
+                    { prefix: 'https://api.twitter.com/1.1/' },
+                );
+            }
+        });
+    });
+}
